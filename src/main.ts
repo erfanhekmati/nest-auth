133d88097fe89176cda79a531157ade4e30184e8
Erfan Hekmati <erfanhekmati8@gmail.com>
2023-08-30 14:19:11 +0330

    Configuration file is created and used for app port.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  // Creating Application
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  // Get configuration service from app
+  const configService = app.get(ConfigService);
+  
+  // Listening on retrieved port
+  await app.listen(configService.get("port"));
+
+  // Logging listening message
+  Logger.log(`App is listening on port ${configService.get("port")} ...`, 'Bootstrap')
 }
 bootstrap();
